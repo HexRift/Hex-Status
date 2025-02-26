06e6475a971bb38cca400f5a0329a7b43ce3822c
Hexarion <me@hexarion.net>
2025-02-26 07:06:46 -0500

    Discontinued replaced by Hex Status 2.0

--- Hex Status Unobf/system/models/BotStatus.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const BotStatusSchema = new mongoose.Schema({
-    status: String,
-    type: String,
-    timestamp: Date
-});
-
-module.exports = mongoose.model('BotStatus', BotStatusSchema);
--- Hex Status Unobf/system/models/index.js
+++
@@ -1,9 +0,0 @@
-const Service = require('./Service');
-const Admin = require('./Admin');
-const StatusMessage = require('./StatusMessage');
-
-module.exports = {
-    Service,
-    Admin,
-    StatusMessage
-};
